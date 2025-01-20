e2d205def722946b47a8ce221c84e19d3ca4941c
W-11 <you@example.com>
2025-01-20 11:13:02 +0330

    fix

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,7 +1,6 @@
 
 // const apiUrl="http://wapi.chipyab.ir"
 const apiUrl="https://sapi.sanecomputer.com"
-// const apiUrl="https://cyapi.chipyab.com"
 // const apiUrl="https://api.toloenajm.ir"
 // const apiUrl="http://192.168.0.194:8181"
 // const apiUrl="http://njweb:3030"
